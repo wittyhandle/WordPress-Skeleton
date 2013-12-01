6ba737fe3ddf91cc8bd9c4cdb119634a58a7bf71
wittyhandle <mikeottinger@gmail.com>
2013-11-30 16:41:27 -0800

    added the markup for the storytk splash page

---
+++ site/content/themes/bones/page-homepage.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Template Name: Home Page
+*/
+?>
+
+<?php get_header(); ?>
+	<img src="http://storytk.com/images/splash.png" usemap="#contact"/>
+	<map name="contact">
+		<area shape="rect" coords="710,750,575,730" href="mailto:carl@storytk.com" alt="Contact">
+	</map>
+	<div class="blue"></div>
+	
+	<div class="incognito">
+		Everyone has a story
+		Some stories center on innovative products...that are imagined, designed, and built by brilliant characters.
+		Some involve groundbreaking research...that’s enabled by vision, intelligence, and toil.
+		Some stories light the way toward...exploiting new opportunities, or solving intractable problems, or dethroning the status quo.
+		Some stories demonstrate leadership, longevity, or perseverance.
+		Some stories are just big old hairballs waiting to be figured out.
+		What’s your story?
+	</div>
+<?php get_footer(); ?>
+
