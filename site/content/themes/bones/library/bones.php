62dfb5ad2d705c38441022a2cd8536eacf30258e
wittyhandle <mikeottinger@gmail.com>
2013-12-03 21:15:32 -0800

    hide the editor from the home page on the admin

--- site/content/themes/bones/library/bones.php
+++ site/content/themes/bones/library/bones.php
@@ -381,6 +381,26 @@ function load_wpa_storytk_lib()
 	wp_enqueue_script( 'wpalchemy-storytk-lib' );
 }
 
+/**
+* If the current post being shown on the admin is for the home page template, remove the editor
+* from the page.
+*/
+add_action( 'admin_init', 'hide_editor' );
+function hide_editor()
+{
+	// Get the Post ID.
+	$post_id = $_GET['post'] ? $_GET['post'] : $_POST['post_ID'] ;
+    if( !isset( $post_id ) ) return;
+
+	// Get the name of the Page Template file.
+	$template_file = get_post_meta($post_id, '_wp_page_template', true);
+   
+	if($template_file == 'page-homepage.php')
+	{
+		remove_post_type_support('page', 'editor');
+    }
+}
+
 /**********************
 Ajax
 **********************/
